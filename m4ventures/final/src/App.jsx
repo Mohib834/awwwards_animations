ae47ecee2b4a4c732ffc4648760219dc965b6aa9
Mohib Arshi <mohibarshi@gmail.com>
2023-07-26 15:23:39 +0530

    starter template ready

---
+++ m4ventures/final/src/App.jsx
@@ -0,0 +1,63 @@
+import "./App.css";
+
+import logo from "./assets/logo.svg";
+import arrow from "./assets/arrow.svg";
+import bigArrow from "./assets/big-arrow.svg";
+
+function App() {
+  return (
+    <>
+      <nav id="nav" className="flex">
+        <img src={logo} width={235} />
+        <a
+          href="/"
+          className="cta flex items-center text-[#4bbd7b] text-[19px] font-semibold ml-auto"
+        >
+          Pitch to us
+          <div className="arrow-box ml-[10px] relative top-[1px]">
+            <img src={arrow} width={8} />
+            <img
+              src={arrow}
+              width={8}
+              className="hidden-arrow absolute left-0 top-0 transition-all duration-300"
+            />
+          </div>
+        </a>
+      </nav>
+      <main className="h-screen w-screen">
+        <div
+          id="arrow-container"
+          className="h-screen w-screen overflow-hidden absolute top-0 left-0 flex justify-center items-center -z-10"
+        >
+          <div>
+            {Array(7)
+              .fill("")
+              .map((_, idx) => (
+                <img
+                  key={idx}
+                  src={bigArrow}
+                  className="-rotate-45 mb-16"
+                  width={300}
+                />
+              ))}
+          </div>
+        </div>
+
+        <div className="h-full w-full flex place-items-center justify-center text-center text-[80px] uppercase font-medium leading-[90px] max-w-5xl mx-auto">
+          <h1 className="text-white tracking-wide">
+            Growing seeds <br /> into forests
+          </h1>
+        </div>
+
+        <div className="absolute bottom-[80px] right-0 max-w-[445px]">
+          <p className="text-[21px] leading-[34px] text-white">
+            We are a group of entrepreneurs who support startups to grow and
+            expand globally.
+          </p>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
